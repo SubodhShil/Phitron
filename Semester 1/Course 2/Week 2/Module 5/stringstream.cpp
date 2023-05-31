95ae00ea22783035f0fbc295410d7be80d2a5d46
HomieSubodh <golang602@gmail.com>
2023-05-31 17:21:23 +0600

    UPDATE: Module 5 fully added

--- Semester 1/Course 2/Week 2/Module 5/stringstream.cpp
+++ Semester 1/Course 2/Week 2/Module 5/stringstream.cpp
@@ -6,7 +6,14 @@ int main()
     string s;
     getline(cin, s);
 
-    stringstream ss;
+    /// seperately print each word
+    stringstream ss(s);
+    string word;
+
+    while (ss >> word)
+    {
+        cout << word << endl;
+    }
 
     return 0;
 }
