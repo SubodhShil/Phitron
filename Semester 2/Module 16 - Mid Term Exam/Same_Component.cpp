4b31afd10c48f27fec697941111e5da9e2f89fed
Subodh Chandra Shil <golang602@gmail.com>
2023-08-26 21:56:11 +0600

    Mid term exam added

--- Semester 2/Module 16 - Mid Term Exam/Same_Component.cpp
+++ Semester 2/Module 16 - Mid Term Exam/Same_Component.cpp
@@ -1,8 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 1005;
+const int M = 1005;
 vector<string> graph;
 int n, m;
+int visited[N][M];
+
+bool isSafe(int i, int j)
+{
+    return (i >= 0 && i < n && j >= 0 && j < m);
+}
+
+void dfs(int si, int sj)
+{
+    if (!isSafe(si, sj) || visited[si][sj] || graph[si][sj] == '-')
+        return;
+
+    visited[si][sj] = 1;
+
+    dfs(si, sj - 1);
+    dfs(si, sj + 1);
+    dfs(si + 1, sj);
+    dfs(si - 1, sj);
+}
 
 int main()
 {
@@ -16,6 +37,13 @@ int main()
 
     int si, sj, di, dj;
     cin >> si >> sj >> di >> dj;
+
+    dfs(si, sj);
+
+    if (visited[di][dj])
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 
     return 0;
 }
