49fdcc3c1f207c8a3e3516d55454cfdfc50bdc3e
Subodh Chandra Shil <golang602@gmail.com>
2023-09-11 18:40:32 +0600

    DP contest added

--- Semester 2/Week 06  - Dynamic Programming/Module 22 - 0_1 Kanpsack/0_1_Knapsack_tabulation.cpp
+++ Semester 2/Week 06  - Dynamic Programming/Module 22 - 0_1 Kanpsack/0_1_Knapsack_tabulation.cpp
@@ -3,28 +3,44 @@ using namespace std;
 
 int main()
 {
+    int t = 0;
+
     int n, capacity;
     cin >> n >> capacity;
     int dp[n + 1][capacity + 1];
     int values[n], weight[n];
 
-    for (int i = 0; i <= n * 2; ++i)
-    {
-        if (i <= n)
-            cin >> values[i];
-        else
-            cin >> weight[i];
-    }
+    for (int i = 0; i < n; ++i)
+        cin >> weight[i];
+
+    for (int i = 0; i < n; ++i)
+        cin >> values[i];
 
     for (int i = 0; i <= n; ++i)
     {
         for (int j = 0; j <= capacity; ++j)
         {
-            /// matches to recursion base case
             if (i == 0 || j == 0)
                 dp[i][j] = 0;
         }
     }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= capacity; ++j)
+        {
+            if (j >= weight[i - 1])
+            {
+                int op1 = dp[i - 1][j - weight[i - 1]] + values[i - 1];
+                int op2 = dp[i - 1][j];
+                dp[i][j] = max(op1, op2);
+            }
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    cout << dp[n][capacity] << endl;
 
     return 0;
 }
