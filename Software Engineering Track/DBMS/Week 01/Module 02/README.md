6c6f5e72e501abf486f24ae0cb0024c1509d3770
Subodh Chandra Shil <golang602@gmail.com>
2023-11-20 22:31:19 +0600

    SQL module 2 donet

--- Software Engineering Track/DBMS/Week 01/Module 02/README.md
+++ Software Engineering Track/DBMS/Week 01/Module 02/README.md
@@ -7,3 +7,44 @@ Query or queries are some instructions made to a database software to find and r
 
 ## Query Language
 ![](query_languages.png)
+
+## SQL vs MySQL
+MySQL is a database whereas SQL is a language. MySQL uses SQL language to write the queries. 
+
+## MySQL datatypes
+
+
+## MySQL Queries
+
+1. Creating a new database
+```sql
+CREATE DATABASE db_name;
+```
+**Caution**: if the database exists with the same name, it will throw an error. 
+
+2. Use an existing database
+```sql
+USE db_name;
+```
+
+3. Table creation
+```sql 
+CREATE TABLE table_name;
+```
+
+4. Deleting an existing table
+```sql
+DROP TABLE table_name;
+```
+
+5. Assign a primary key: To uniquley identify a table we should always provide a primary key to any suitable field.
+```sql
+CREATE TABLE
+    Students(
+        Roll CHAR(4) PRIMARY KEY, -- 
+        Name VARCHAR(50),
+        Marks DOUBLE
+    );
+```
+
+### **Duplicate values aren't allowed for primary key field.**
---
+++ Software Engineering Track/DBMS/Week 01/Module 02/test.sql
@@ -0,0 +1,48 @@
+-- The query let us use an existing table from the database
+USE test;
+
+-- Query for creating a new table in the database
+CREATE TABLE
+    Students(
+        Roll CHAR(4) PRIMARY KEY, -- Primary key is the unique  identifier 
+        Name VARCHAR(50),
+        Marks DOUBLE
+    );
+
+-- Insert field values
+
+-- Way 01
+INSERT INTO Students 
+(`Roll`, `Name`, `Marks`) VALUES(33, 'Subodh', 77);
+
+-- Way 02
+INSERT INTO Students VALUES(1, "Roktim", 99);
+
+-- Updating value
+-- While updating values one should turn off the safe mode
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE students
+SET Name = 'Roktim'
+WHERE `Roll` = 1;
+
+-- After updating the values safe mode should turn on
+SET SQL_SAFE_UPDATES = 1;
+
+-- Query for deleting an existing TABLE
+DROP TABLE Students;
+
+-- Delete values
+DELETE FROM students
+WHERE `Roll` = 1;
+
+-- Show all available tables
+SHOW TABLES;
+
+-- To see the table structure
+DESCRIBE Students;
+-- or 
+EXPLAIN Students;
+
+-- To display a specific table
+SELECT * FROM students;
